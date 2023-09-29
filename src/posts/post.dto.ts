203f23cf836d44562bab215c8499f9ae21c658fa
Taiyo Maruyama <stickytomatobaby0501@gmail.com>
2023-09-29 12:51:56 +0900

    getリクエストにHttpExceptionを追加

--- src/posts/post.dto.ts
+++ src/posts/post.dto.ts
@@ -1,9 +1,8 @@
 export class PostDto {
   userName: string;
-  officialBudge: boolean;
   userId: string;
   // todo: APIのレスポンス次第で変更
-  postedDuration: string;
+  postedAt: Date;
   text: string;
   // bottomActionの有無はそれぞれのテーブルからidがら返却されたかどうかで判断
   reply?: string;
